7492cbc8dad34efb14d4083df57ace0f5507f683
momo5181 <sam20051012@gmail.com>
2023-11-14 09:53:05 +0800

    我自己的射擊
    多敵人、雄槍

---
+++ Assets/script/Target.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Target : MonoBehaviour
+{
+    public float health = 50f;//設定血量
+    public void TakeDamage (float amount)
+    {
+        health -=amount;
+        if(health <= 0f) 
+        {
+            Die();
+          
+            
+        }
+    }
+  
+    
+    void Die()
+    {
+        Destroy(gameObject);//銷毀物體
+    }
+ 
+}
